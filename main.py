4f957e50f91dcdc96d2319cfa0d5de4f6ef46f6d
TIGERCHANG123 <43056612+TIGERCHANG123@users.noreply.github.com>
2020-02-27 16:37:47 +0800

    change root

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tensorflow.compat.v1 import InteractiveSession
 
 ubuntu_root='/home/tigerc'
 windows_root='D:/Automatic/SRTP/GAN'
-root = ubuntu_root
+root = '/content/drive/My Drive/my_network'
 temp_root = root+'/temp'
 
 def main(continue_train, train_time):
