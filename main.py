2b680ee7d305a59aab630a329ef3df60e49fe858
TIGERCHANG123 <2462924814@qq.com>
2020-02-28 13:50:11 +0800

    EBGAN

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ from tensorflow.compat.v1 import InteractiveSession
 
 ubuntu_root='/home/tigerc'
 windows_root='D:/Automatic/SRTP/GAN'
-# root = '/content/drive/My Drive/temp'
-root = ubuntu_root
+root = '/content/drive/My Drive/temp'
+# root = ubuntu_root
 temp_root = root+'/temp'
-dataset_root = root
+dataset_root = '/content'
 
 def main(continue_train, train_time, train_epoch):
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # or any {'0', '1', '2'}
